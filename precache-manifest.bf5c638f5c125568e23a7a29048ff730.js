8ae53731fda46b6844b3286f6a31d7277b6b916f
GetNekt <37420954+GetNekt@users.noreply.github.com>
2019-10-18 11:42:55 +0100

    deployed site

---
+++ precache-manifest.bf5c638f5c125568e23a7a29048ff730.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08d77b173a07c5b3247f49e2699d3e36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8db5771bc65e031f6f7",
+    "url": "/static/css/2.0981ad52.chunk.css"
+  },
+  {
+    "revision": "ce956386a110a333519b",
+    "url": "/static/css/main.f022e02f.chunk.css"
+  },
+  {
+    "revision": "b8db5771bc65e031f6f7",
+    "url": "/static/js/2.8c061c09.chunk.js"
+  },
+  {
+    "revision": "ce956386a110a333519b",
+    "url": "/static/js/main.27b05a26.chunk.js"
+  },
+  {
+    "revision": "c869eb6615fffef47657",
+    "url": "/static/js/runtime-main.2d8e7ae3.js"
+  },
+  {
+    "revision": "8d7ae5ac6e934401e57e82f70b996bc1",
+    "url": "/static/media/ab-img.8d7ae5ac.png"
+  },
+  {
+    "revision": "c153b3445d2adcee6ed18285f04208b8",
+    "url": "/static/media/book.c153b344.png"
+  },
+  {
+    "revision": "59130505fa65131e3dd5f2a4bd24d212",
+    "url": "/static/media/gamepad.59130505.png"
+  },
+  {
+    "revision": "35b71ebbd88d1e50aab5f3cd3aa05b63",
+    "url": "/static/media/gdg-google-developer-group-logo.35b71ebb.svg"
+  },
+  {
+    "revision": "6c580a7002b88abd40aef62a3b6bf496",
+    "url": "/static/media/icon.6c580a70.ico"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "a3dff246d77211de05dd6a598ee0b094",
+    "url": "/static/media/map-color-overlay.a3dff246.png"
+  },
+  {
+    "revision": "e7e275df8340cc24e5c6b08eb7dc5946",
+    "url": "/static/media/me.e7e275df.JPG"
+  }
+]);
